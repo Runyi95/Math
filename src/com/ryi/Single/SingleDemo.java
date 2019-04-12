5f9d8fd85bb4397d88695a82ffe81db9f9722542
Runyi <xiaoyiaijiang@163.com>
2019-04-12 14:43:36 +0800

    NO 2

--- src/com/ryi/Single/SingleDemo.java
+++ src/com/ryi/Single/SingleDemo.java
@@ -8,5 +8,7 @@ public class SingleDemo {
     public static void main(String[] args){
         SingleObject object = SingleObject.getInstance();
         object.showMessage();
+
+        //121545132541
     }
 }
